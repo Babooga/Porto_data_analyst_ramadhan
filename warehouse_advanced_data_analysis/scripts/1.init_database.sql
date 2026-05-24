0a0e870a35cdfc0e31f7d0ae9357413ab754bbbe
Baobo <140949569+Babooga@users.noreply.github.com>
2026-05-24 18:14:33 +0700

    Add files via upload

---
+++ warehouse_advanced_data_analysis/scripts/1.init_database.sql
@@ -0,0 +1,14 @@
+-- Create the 'DataWarehouse' database
+create database DataWarehouse;
+
+
+USE DataWarehouse;
+go
+
+-- Create Schemas
+CREATE SCHEMA bronze;
+go
+CREATE SCHEMA silver;
+go
+CREATE SCHEMA gold;
+
